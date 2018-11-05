cbd5f473aad2eb8d06e841106fbafd3dcaec486d
robzz <robin94320@gmail.com>
2018-11-05 14:04:17 +0100

    add picture display activity

--- app/src/main/java/epitech/epicture/GalleryItemActivity.java
+++ app/src/main/java/epitech/epicture/GalleryItemActivity.java
@@ -2,6 +2,10 @@ package epitech.epicture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class GalleryItemActivity extends AppCompatActivity {
 
@@ -11,6 +15,26 @@ public class GalleryItemActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gallery_item);
 
         GalleryItem item = (GalleryItem) getIntent().getSerializableExtra("item");
-        System.out.print(item);
+        ImageView image = (ImageView) findViewById(R.id.galleryItemView);
+
+        String[] array = item.getImages()[0].getType().split("/");
+        if (array[0].equals("image")) {
+            String url = item.getImages()[0].getLink();
+            Picasso.get()
+                    .load(url)
+                    .resize(360, 240)
+                    .centerInside()
+                    .into(image);
+        } else
+            Picasso.get()
+                    .load("http://i.imgur.com/DvpvklR.png")
+                    .resize(360, 240)
+                    .centerInside()
+                    .into(image);
+
+        String title = item.getTitle();
+        TextView textView = (TextView) findViewById(R.id.galleryItemTitle);
+        textView.setText(title);
+
     }
 }
